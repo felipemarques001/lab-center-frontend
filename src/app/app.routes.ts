9c35b9b55cf22042fd15624338efe5e892885475
Felipe Marques <felipemarquesgg@outlook.com>
2024-07-01 20:01:40 -0300

    feat(login): finalizando a estrutura inicial da página login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 export const routes: Routes = [
-    { path: '', component: LoginComponent }
+    { path: '', component: LoginPageComponent }
 ];
