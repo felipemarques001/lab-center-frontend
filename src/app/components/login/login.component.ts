5291e496bbc86986463c631e5756987d19869474
Felipe Marques <felipemarquesgg@outlook.com>
2024-07-01 15:18:04 -0300

    style(login): organizando o HTML e CSS de LoginComponent

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  onFormSubmit(formControl: NgForm): void {
+    console.log(formControl);
+  }
 }
